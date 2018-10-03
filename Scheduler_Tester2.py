ac54c33e072f02023e69deeba87c325ec6173deb
ProgramInc <43065474+ProgramInc@users.noreply.github.com>
2018-10-03 13:24:59 +0300

    Update Scheduler_Tester2.py

--- Scheduler_Tester2.py
+++ Scheduler_Tester2.py
@@ -5,55 +5,40 @@ from WorkScheduler10 import Employee, DaysAndShifts
 class SchedulerTester(unittest.TestCase):
 
     def setUp(self):
-        self.itay = Employee(name='Itay', shift_count=5, cooldown=0, scheduled_shifts=None, protools_authorized=True)
-        self.sundayProtools = DaysAndShifts(day_name='Sunday', shift_name='Protools', assigned_employees=None)
-        self.sundayProtools.assign_employee(self.itay)
-        self.itay.cooldown = 3
+        self.anEmployee1 = Employee(name='employee1', shift_count=5, cooldown=0, scheduled_shifts=[], is_protools_authorized=True)
+        self.anEmployee2 = Employee(name='employee2', shift_count=3, cooldown=0, scheduled_shifts=[], is_protools_authorized=False)
+        self.aShift1 = DaysAndShifts(day_name='aDay1', shift_name='aShift1', assigned_employees=[])
+        self.aShift2 = DaysAndShifts(day_name='aDay2', shift_name='aShift2', assigned_employees=[])
+        self.aShift1.assign_employee(self.anEmployee1)
+        self.anEmployee1.cooldown = 3
 
 
     def tearDown(self):
-        self.sundayProtools.assigned_employees = None
-        self.itay.cooldown = 0
+        pass
 
-    def test_that_sundayProtools_assigned_employees_is_not_an_empty_list(self):
-        """This test will verify that the list sundayProtools.assigned_employees is not empty - This test is meant to pass"""
-        self.assertGreater(len(self.sundayProtools.assigned_employees), 0)
+    def test_that_aShift1_dot_assigned_employees_is_not_an_empty_list(self):
+        """This test will verify that the list sundayProtools.assigned_employees is not empty"""
+        self.assertGreater(len(self.aShift1.assigned_employees), 0)
 
 
-    def test_sunday_protools_is_correct_employee_amount(self):
+    def test_aShift1_is_correct_employee_amount(self):
         """This test checks if the method sundayProtools.assign_employee() outputs the correct
-        amount of employees - This test is meant to pass"""
-        self.assertEqual(len(self.sundayProtools.assigned_employees), 1)
+        amount of employees"""
+        self.aShift1.assign_employee(self.anEmployee2)
+        self.assertEqual(len(self.aShift1.assigned_employees), 2)
 
 
     def test_sunday_protools_change_expected_result_and_test_again(self):
         """This test adds another employee to the list sundayProtools.assigned_employees and then
-        checks if the amount is correct - This test is meant to raise an AssertionError and pass"""
-        self.sundayProtools.assigned_employees.append('Zemer')
+        checks if the amount is correct - This test is meant to raise an AssertionError"""
+        self.aShift1.assigned_employees.append(self.anEmployee2)
         try:
-            self.assertEqual(len(self.sundayProtools.assigned_employees), 1)
+            self.assertEqual(len(self.aShift1.assigned_employees), 1)
         except AssertionError:
                 pass
     #
     def test_is_employee_qualified_for_protools_shift(self):
-        '''This test will check whether or not the employee that was assigned is in the
-        list of qualified employees for a protools shift - This test is meant to pass'''
-        for employee in self.sundayProtools.assigned_employees:
-            self.assertTrue(employee.protools_authorized)
-    #
-    # # @unittest.skip('ruins the other tests')
-    # def test_is_an_unqualified_employee_qualified_for_protools_shift(self):
-    #     '''This test adds an unqualified employee to the list SundayProtools and will then
-    #     check whether or not the employee that was assigned is in the
-    #     list of qualified employees for a protools shift - This test is meant to fail'''
-    #     WorkScheduler8.SundayProtools.append('Zemer')
-    #     for name in WorkScheduler8.SundayProtools:
-    #         self.assertIn(name, (WorkScheduler8.proTools))
-    #
-    # def test_is_any_name_listed_twice_in_sundayProtools(self):
-    #     '''This test checks if any name appears more than once in SundayProotls.
-    #     The logic behind the test is that it uses the SET propeties(no duplicates) and then
-    #     complares the length of the list SundayProtools to the lengh of the set(SundayProtools)
-    #     '''
-    #     self.assertEqual(len(WorkScheduler8.SundayProtools), len(list(set(WorkScheduler8.SundayProtools))))
-    #
+        """This test will check whether or not the employee that was assigned is in the
+        list of qualified employees for a protools shift """
+        for employee in self.aShift1.assigned_employees:
+            self.assertTrue(employee.is_protools_authorized)
