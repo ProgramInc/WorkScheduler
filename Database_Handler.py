f0a59ab8885037021dd0e85734f0f6e9eeef401e
ProgramInc <43065474+ProgramInc@users.noreply.github.com>
2019-02-06 01:54:45 +0200

    redundant. entire file was moved into main code to avoid importing issues

--- Database_Handler.py
+++ Database_Handler.py
@@ -1,6 +1,7 @@
 import mysql.connector as sqlcon
 import WorkScheduler10
 
+
 class DbHandler:
 
     def __init__(self):
@@ -42,19 +43,19 @@ class DbHandler:
     # def db_get_random_employee(self):
     #     self
 
-    def db_select_employee(self, authorization):
-        self.cursor.execute('SELECT FROM employee_authorization')
+    # def db_select_employee(self, authorization):
+    #     self.cursor.execute('SELECT FROM employee_authorization')
 
-    def db_get_authorizations(self):
-        authorization_dictionary = {}
-        self.cursor.execute('SELECT * FROM authorization')
-        data = self.cursor.fetchall()
-        # print(data)
-        # for row in data:
-        #     print(row)
-        for a, b in data:
-            authorization_dictionary[a] = b
-        print(authorization_dictionary)
+    # def db_get_authorizations(self):
+    #     authorization_dictionary = {}
+    #     self.cursor.execute('SELECT idauthorization, authorization_name FROM authorization')
+    #     data = self.cursor.fetchall()
+    #     # print(data)
+    #     # for row in data:
+    #     #     print(row)
+    #     for a, b in data:
+    #         authorization_dictionary[a] = b
+    #     print(authorization_dictionary)
 
     def db_get_employees(self):
         self.cursor.execute('SELECT idemployee, employee_first_name, employee_last_name,'
@@ -74,7 +75,33 @@ class DbHandler:
             for a, b in data:
                 if employee.idemployee == a:
                     employee.authorization = b
-                    
+
+    def db_employee_limits(self):
+        self.cursor.execute('SELECT idemployee, idshift FROM employee_limit')
+        data = self.cursor.fetchall()
+        for employee in WorkScheduler10.all_employees:
+            for a, b in data:
+                if employee.idemployee == a:
+                    employee.employee_limits.append(b)
+
+
+
+    def db_get_days(self):
+        self.cursor.execute('SELECT idday, day_name FROM day')
+        data = self.cursor.fetchall()
+        for a, b in data:
+            print(a, b)
+            WorkScheduler10.Days(a, b)
+        print(WorkScheduler10.all_days)
+
+    def db_get_shifts(self):
+        self.cursor.execute('SELECT idshift, shift_name, idday, shift_required_employees,'
+                            'shift_required_authorizations FROM shift')
+        data = self.cursor.fetchall()
+        for a, b, c, d, e in data:
+            WorkScheduler10.Shifts(a, b, c, d, e)
+        print(WorkScheduler10.all_shifts)
+
 
 
     def __del__(self):
@@ -85,10 +112,11 @@ class DbHandler:
 handler = DbHandler()
 # print(allemployees)
 # handler.db_insert_employee('emek', 'netanel')
-handler.show_all_employees()
+# handler.show_all_employees()
 # handler.db_delete_employee_by_id(ids=2)
 # handler.db_delete_employee_by_name('emek', 'netanel')
 # handler.show_all_employees()
-handler.db_get_authorizations()
-handler.db_get_employees()
-handler.db_employee_authorizations()
+
+
+
+
